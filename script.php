68b433701c022bcf4c61a86b73c43035b196fb2d
mikhalich89 <s.m.leshukov@gmail.com>
2020-03-20 17:41:17 +0700

    work begin, view json massive

--- script.php
+++ script.php
@@ -1,29 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-</head>
-<body>
-
+<?php
+// Устанавливаем кодировку
+header('Content-Type: text/html; charset=utf-8');
+?>
 <!--Контейнер для новостей-->
 <div class="t-container news_container">
 
 
-	<?php
-	//Опции для функции stream_context_create();
-	$contextOptions = array (
-		"ssl" => array (
-			"verify_peer" => false,
-			"verify_peer_name" => false
-		)
-	);
-	//Путь к json
-	$jsonPath = 'https://news.sfu-kras.ru/json/city';
-	//Получение содержимого json
-	$jsonSelf = file_get_contents($jsonPath, false, stream_context_create($contextOptions));
-	//Преобразование json в удобоиспользуемый вид
-	$jsonCvt = json_decode($jsonSelf, true);
+  <?php
+
+  //Ключевое слово для отладки
+  $rubricName = "открытый университет образование";
+
+
+
+  //Опции для функции stream_context_create();
+  $contextOptions = array (
+    "ssl" => array (
+      "verify_peer" => false,
+      "verify_peer_name" => false
+    )
+  );
+  //Путь к json
+  $jsonPath = 'https://news.sfu-kras.ru/json/city';
+  //Получение содержимого json
+  $jsonSelf = file_get_contents($jsonPath, false, stream_context_create($contextOptions));
+  //Преобразование json в удобоиспользуемый вид
+  $jsonCvt = json_decode($jsonSelf, true);
   // Определяем ключевое слово для проверки
   $keyWord = $rubricName;
   // Определяем длинну массива
@@ -32,9 +34,13 @@
   for ($i=0; $i < $arrLength; $i++) {
     //Осуществляем вывод массива по условию
     if (in_array($keyWord, $jsonCvt['items'][$i]['tags'])) {
+      echo "<pre>";
+      print_r($jsonCvt['items'][$i]);
+      echo "</pre>";
+      /*
   ?>
 
-  <!--Вывод новости-->
+  <!--Вывод новости
   <div class="t649__col t-col t-col_4 t-align_left t-item news_item">
     <a class="t649__linkwrapper" href="<?php echo $jsonCvt['items'][$i]['link'] ?>" target="_blank">
       <div class="t649__blockimg t649__blockimg_3-2 t-bgimg" data-original="<?php echo $jsonCvt['items'][$i]['poster'] ?>" style="background: url(&#39;static/08393c30a3e0be1cdffa12659f39828b_2019.jpg&#39;) center center no-repeat; background-size:cover; margin: -6px;">
@@ -51,13 +57,15 @@
       </div>
     </a>
   </div>
+  -->
 
-  <?php }} ?>
+  <?php */}} ?>
 
 </div>
 
 <!--Стили для новости-->
 <style type="text/css">
+  /*
   .news_container {
     column-count: 1;
     column-gap: ;
@@ -75,9 +83,5 @@
     .news_container {
       column-count: 3;
     }
-  }
+  }*/
 </style>
-
-  
-</body>
-</html>
