4ef6ca2f8b3d706279ffa3bbdf0000965448de0d
mikhalich89 <s.m.leshukov@gmail.com>
2020-03-24 16:41:48 +0700

    code refactoring and finish + comments for my self

--- script.php
+++ script.php
@@ -2,135 +2,81 @@
 // Устанавливаем кодировку
 header('Content-Type: text/html; charset=utf-8');
 ?>
+
 <!--Контейнер для новостей-->
 <div class="t-container news_container">
 
+<?php
 
-	<?php
+// Скрипт отображает новости по двум условиям:
+// 1. Рубрика.
+//    Если массив тегов не содержит ключевого слова, то новость отображаться не будет
+//    !Если массив тегов содержит ключевое слово открытый университет рубрика, то новость будет отображаться в соответствующей рубрике
+// 2. Дата.
+//    Скрипт отображает только те новости, которые снабжены ключевым словом date=11062020
+//    Где date - переменная содержащая дату 11 июня 2020 года
+//    Если дата не указана, новость отображаться не будет
+//    Если указанная в ключевом слове дата меньше текущей, новость отображаться не будет
+//    !Если указанная в ключевом слове дата больше текущей, новость будет отображаться
+// 3. Таким образом, новость будет отображаться в соответствующей рубрике, если указано ключевое слово и дата больше текущей
 
-  //Ключевое слово для отладки
-	$rubricName = "открытый университет образование";
+// Ключевое слово для отладки
+$rubric_name = "открытый университет образование";
 
+// Опции для функции stream_context_create();
+$context_options = array (
+	"ssl" => array (
+		"verify_peer" => false,
+		"verify_peer_name" => false
+	)
+);
 
+//Путь к json
+$json_path = 'https://news.sfu-kras.ru/json/city';
+// Получение содержимого json
+$json = file_get_contents($json_path, false, stream_context_create($context_options));
+// Преобразование json в удобоиспользуемый вид
+$arr_news = json_decode($json, true);
+// Определяем ключевое слово для проверки
+$key_word = $rubric_name;
+// Определяем длинну массива элементов, каждый из которых содержит данные для формирования новости
+$arrLength = count($arr_news['items']);
+// Текущая дата
+$current_time = date('dmY');
 
-  //Опции для функции stream_context_create();
-	$contextOptions = array (
-		"ssl" => array (
-			"verify_peer" => false,
-			"verify_peer_name" => false
-		)
-	);
-  //Путь к json
-	$jsonPath = 'https://news.sfu-kras.ru/json/city';
-  //Получение содержимого json
-	$jsonSelf = file_get_contents($jsonPath, false, stream_context_create($contextOptions));
-  //Преобразование json в удобоиспользуемый вид
-	$jsonCvt = json_decode($jsonSelf, true);
-  // Определяем ключевое слово для проверки
-	$keyWord = $rubricName;
-  // Определяем длинну массива элементов, каждый из которых содержит данные для формирования новости
-	$arrLength = count($jsonCvt['items']);
-  // Текущая дата
-  $current_time = date('dmY');
-  // Отладка
-	var_dump($arrLength);
+// Обходим массив элементов
+foreach ($arr_news['items'] as $key_i => $value_i) {
+  // Если в массиве тэгов массива элементов есть совпадение, то
+  if (in_array($key_word, $value_i['tags'])) {
+    // Обходим массив тегов, чтобы получить дату
+    foreach ($value_i['tags'] as $key_t => $value_t) {
+      // Если строка массива содержит вхождение, то
+      if (strstr($value_t, 'date')) {
+        // Извлекаем заложенную в неё переменную
+        parse_str($value_t);
+        // Если дата больше текущей даты, то выводим этот айтем
+        if ($date > $current_time) {
+          // echo "<pre>";
+          // print_r($value_i);
+          // echo "</pre>";
+?>
 
-
-  // echo "<pre>";
-  // print_r($current_time);
-  // echo "</pre>";
-
-
-
-
-
-
-
-
-
-	
-  // Обходим массив элементов в цикле
-	for ($i=0; $i < $arrLength; $i++) {
-  	// Если тэги массива для формирования новости содержат ключевое слово, то работаем с ним, то есть формирование новости производим из него
-		if (in_array($keyWord, $jsonCvt['items'][$i]['tags'])) {
-    	//Отладка
-			// echo "<pre>";
-			// print_r($jsonCvt['items'][$i]['tags']);
-			// echo "</pre>";
-      	// Находим ключ строки по вхождению подстроки в эту строку
-      	// Для этого обходим тэги
-			foreach ($jsonCvt['items'][$i]['tags'] as $key => $value) {
-      		// Если значение элемента тега содержит вхождение подстроки, то работаем с ним
-				if (strstr($value, 'date')) {
-    			// Присваиваем в переменную значение строки, найденное по ключу
-					$str = $jsonCvt['items'][$i]['tags'][$key];
-    			// Парсим строку с помощью функции в результате получаем переменную со значением
-					parse_str($str);
-    			//Отладеа
-					// echo "<pre>";
-					// print_r($date);
-					// echo "</pre>";
-          if ($date > $current_time) {
-            // echo "<pre>";
-            // print_r($jsonCvt['items'][$i]); // Этот адрес и будет материалом для формирования новости
-            // echo "</pre>";
-
-
-
-      // array_keys($array, value) Возвращает ключ(и) массива по значению
-      // array_search(value, $array) Возвращает ключ первого найденного элемента по значению
-      // in_array(valie, $array) Возвращает true если значение было найдено в массиве
-      // array_values($array) Возвращает заново индексированный массив значений
-      // array_walk($array, 'function') Применяет функцию к каждому элементу массива
-      // count($array) Подсчитывает количество элементов массива или чего-либо в объекте
-      // current($array) Возвращает текущий элемент массива
-      // next($array) Возвращает следующий элемент массива
-      // prev($array) Возвращает предыдущий элемент массива
-      // end($array) Возвращает последний элемент массива
-      // key($array) Возвращает индекс текущего элемента массива
-
-      // explode(delimiter, string) Разбивает строку с помощью разделителя!
-      // parse_str() // Разбирает строку в переменные
-      // stripos(haystack, needle) Возвращает позицию первого вхождения подстроки без учета регистра
-      // strpos(haystack, needle) Возвращает позицию первого вхождения подстроки
-      // strlen(string) Возвращает длинну строки
-      // strstr(haystack, needle) Находит первое вхождение подстроки
-
-
-
-
-
-
-      //Сначала найти, а потом взорвать
-
-      //date_parse_from_format(format, date) // Получение массива от даты, заданной в определенном формате
-
-
-
-
-
-
-
-
-
-  ?>
-
-  <div class="t649__col t-col t-col_4 t-align_left t-item news_item">
-    <a class="t649__linkwrapper" href="<?php echo $jsonCvt['items'][$i]['link'] ?>" target="_blank">
-      <div class="t649__blockimg t649__blockimg_3-2 t-bgimg" data-original="<?php echo $jsonCvt['items'][$i]['poster'] ?>" style="background: url(&#39;static/08393c30a3e0be1cdffa12659f39828b_2019.jpg&#39;) center center no-repeat; background-size:cover; margin: -6px;">
-      </div>
-      <div class="t649__textwrapper">
-        <div class="t649__sp"></div>
-        <div class="t649__title t-heading t-heading_sm" style="font-size:30px;font-weight:300;font-family:&#39;Roboto&#39;;" field="li_title__1489060571494"><?php echo $jsonCvt['items'][$i]['title'] ?></div>
-        <div class="t649__text t-text t-text_sm" style="" field="li_descr__1489060571494"><?php echo $jsonCvt['items'][$i]['text'] ?></div>
-        <div class="t649__btn-container">
-          <div class="t649__btn-wrapper">
-            <div class="t649__submit t-btn t-btn_xs" style="color:#ffffff;background-color:#000000;border-radius:4px; -moz-border-radius:4px; -webkit-border-radius:4px;">Подробнее</div>
-          </div>
+<div class="t649__col t-col t-col_4 t-align_left t-item news_item">
+  <a class="t649__linkwrapper" href="<?php echo $value_i['link'] ?>" target="_blank">
+    <div class="t649__blockimg t649__blockimg_3-2 t-bgimg" data-original="<?php echo $value_i['poster'] ?>" style="background: url(&#39;static/08393c30a3e0be1cdffa12659f39828b_2019.jpg&#39;) center center no-repeat; background-size:cover; margin: -6px;">
+    </div>
+    <div class="t649__textwrapper">
+      <div class="t649__sp"></div>
+      <div class="t649__title t-heading t-heading_sm" style="font-size:30px;font-weight:300;font-family:&#39;Roboto&#39;;" field="li_title__1489060571494"><?php echo $value_i['title'] ?></div>
+      <div class="t649__text t-text t-text_sm" style="" field="li_descr__1489060571494"><?php echo $value_i['text'] ?></div>
+      <div class="t649__btn-container">
+        <div class="t649__btn-wrapper">
+          <div class="t649__submit t-btn t-btn_xs" style="color:#ffffff;background-color:#000000;border-radius:4px; -moz-border-radius:4px; -webkit-border-radius:4px;">Подробнее</div>
         </div>
       </div>
-    </a>
-  </div>
+    </div>
+  </a>
+</div>
 
 <?php }}}}} ?>
 
@@ -138,12 +84,10 @@ header('Content-Type: text/html; charset=utf-8');
 
 <!--Стили для новости-->
 <style type="text/css">
-  /*
   .news_container {
     column-count: 1;
     column-gap: ;
   }
-
   .news_item {
     break-inside: avoid;
     margin-bottom: 40px;
@@ -151,10 +95,9 @@ header('Content-Type: text/html; charset=utf-8');
     border-radius: 4px;
     padding: 5px;
   }
-
   @media (min-width: 961px) {
     .news_container {
       column-count: 3;
     }
-    }*/
+  }
 </style>
