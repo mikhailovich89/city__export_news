e155de0718002b5376e1e13794d67202fdc0c2f5
mikhalich89 <s.m.leshukov@gmail.com>
2020-03-25 16:18:00 +0700

    only functional, no comments, no help, + new finctions

--- script.php
+++ script.php
@@ -1,49 +1,58 @@
 <div class="t-container news_container">
-  
-<?php
 
-$context_options = array (
-	"ssl" => array (
-		"verify_peer" => false,
-		"verify_peer_name" => false
-	)
-);
+  <?php
+  $context_options = array (
+   "ssl" => array (
+    "verify_peer" => false,
+    "verify_peer_name" => false
+  )
+  );
 
-$json_path = 'https://news.sfu-kras.ru/json/city';
-$json = file_get_contents($json_path, false, stream_context_create($context_options));
-$arr_news = json_decode($json, true);
-$key_word = $rubric_name;
-$current_time = date('dmY');
+  $json_path = 'https://news.sfu-kras.ru/json/city';
+  $json = file_get_contents($json_path, false, stream_context_create($context_options));
+  $arr_news = json_decode($json, true);
+  $key_word = $rubric_name;
+  $current_date = date('d-m-Y');
 
-foreach ($arr_news['items'] as $key_i => $value_i) {
-  if (in_array($key_word, $value_i['tags'])) {
-    foreach ($value_i['tags'] as $key_t => $value_t) {
-      if (strstr($value_t, 'date')) {
-        parse_str($value_t);
-        if ($date > $current_time) {
-?>
+  foreach ($arr_news['items'] as $key_i => $value_i) {
+    if (in_array($key_word, $value_i['tags'])) {
+      foreach ($value_i['tags'] as $key_t => $value_t) {
+        if (strstr($value_t, 'date')) {
+          parse_str($value_t);
+          if (strtotime($date) < strtotime($current_date)) {
+            unset($value_i);
+          }
+        }
+      }
+      if ($value_i) {
+        ?>
 
-<div class="t649__col t-col t-col_4 t-align_left t-item news_item">
-  <a class="t649__linkwrapper" href="<?php echo $value_i['link'] ?>" target="_blank">
-    <div class="t649__blockimg t649__blockimg_3-2 t-bgimg" data-original="<?php echo $value_i['poster'] ?>" style="background: url(&#39;static/08393c30a3e0be1cdffa12659f39828b_2019.jpg&#39;) center center no-repeat; background-size:cover; margin: -6px;">
-    </div>
-    <div class="t649__textwrapper">
-      <div class="t649__sp"></div>
-      <div class="t649__title t-heading t-heading_sm" style="font-size:30px;font-weight:300;font-family:&#39;Roboto&#39;;" field="li_title__1489060571494"><?php echo $value_i['title'] ?></div>
-      <div class="t649__text t-text t-text_sm" style="" field="li_descr__1489060571494"><?php echo $value_i['text'] ?></div>
-      <div class="t649__btn-container">
-        <div class="t649__btn-wrapper">
-          <div class="t649__submit t-btn t-btn_xs" style="color:#ffffff;background-color:#000000;border-radius:4px; -moz-border-radius:4px; -webkit-border-radius:4px;">Подробнее</div>
+        <div class="t649__col t-col t-col_4 t-align_left t-item news_item">
+          <a class="t649__linkwrapper" href="<?php echo $value_i['link'] ?>" target="_blank">
+            <div class="t649__blockimg t649__blockimg_3-2 t-bgimg" data-original="<?php echo $value_i['poster'] ?>" style="background: url(&#39;static/08393c30a3e0be1cdffa12659f39828b_2019.jpg&#39;) center center no-repeat; background-size:cover; margin: -6px;">
+            </div>
+            <div class="t649__textwrapper">
+              <div class="t649__sp"></div>
+              <div class="t649__title t-heading t-heading_sm" style="font-size:30px;font-weight:300;font-family:&#39;Roboto&#39;;" field="li_title__1489060571494"><?php echo $value_i['title'] ?></div>
+              <div class="t649__text t-text t-text_sm" style="" field="li_descr__1489060571494"><?php echo $value_i['text'] ?></div>
+              <div class="t649__btn-container">
+                <div class="t649__btn-wrapper">
+                  <div class="t649__submit t-btn t-btn_xs" style="color:#ffffff;background-color:#000000;border-radius:4px; -moz-border-radius:4px; -webkit-border-radius:4px;">Подробнее</div>
+                </div>
+              </div>
+            </div>
+          </a>
         </div>
-      </div>
-    </div>
-  </a>
+
+        <?php
+      }
+    }
+  }
+  ?>
+
 </div>
 
-<?php }}}}} ?>
-
-</div>
-
+<!--Стили для новости-->
 <style type="text/css">
   .news_container {
     column-count: 1;
